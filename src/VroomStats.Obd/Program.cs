74064d52ac3a68e4ac6f4c20b65419b7eeed6f1c
Allan Mercou <allan.mercou@icloud.com>
2022-02-04 20:51:28 +0100

    Adds a simple background service that pulls some random values from the OBDII connection

--- src/VroomStats.Obd/Program.cs
+++ src/VroomStats.Obd/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OBD.NET.Common.Communication;
+using OBD.NET.Common.Devices;
+using OBD.NET.Desktop.Communication;
+using VroomStats.Obd.Services;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((ctx, services) =>
+    {
+        services.AddSingleton<ISerialConnection>(_ => new SerialConnection(ctx.Configuration["Obd:SerialPort"]));
+        services.AddSingleton(x => new ELM327(x.GetRequiredService<ISerialConnection>()));
+        services.AddHostedService<ObdService>();
+    });
+
+var app= builder.Build();
+app.Run();
---
+++ src/VroomStats.Obd/Services/ObdService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OBD.NET.Common.Devices;
+using OBD.NET.Common.OBDData;
+
+namespace VroomStats.Obd.Services;
+
+public class ObdService : BackgroundService
+{
+    private readonly ELM327 _device;
+    private readonly ILogger<ObdService> _logger;
+
+    public ObdService(ELM327 device, ILogger<ObdService> logger)
+    {
+        _device = device;
+        _logger = logger;
+    }
+    
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await _device.InitializeAsync();
+        
+        var fuelType = await _device.RequestDataAsync<FuelType>();
+        var fuelStatus = await _device.RequestDataAsync<FuelSystemStatus>();
+        
+        _logger.LogInformation("Fuel type: {Type}; Status 1: {Status1}; Status 2: {Status2}", 
+            fuelType, fuelStatus.StatusSystem1, fuelStatus.StatusSystem2);
+        
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var speed = await _device.RequestDataAsync<VehicleSpeed>();
+            var rpm = await _device.RequestDataAsync<EngineRPM>();
+            
+            _logger.LogInformation("Car speed: {Speed}; Rpm: {Rpm}", 
+                speed, rpm);
+
+            // pull every 500ms
+            await Task.Delay(500, stoppingToken);
+        }
+    }
+}
