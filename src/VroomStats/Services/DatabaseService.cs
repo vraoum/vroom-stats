a895b205dd6c0b2d804aa7efe580e9b869b37577
Allan Mercou <allan.mercou@icloud.com>
2022-02-23 22:01:48 +0100

    Makes mutable database calls thread safe.

--- src/VroomStats/Services/DatabaseService.cs
+++ src/VroomStats/Services/DatabaseService.cs
@@ -9,9 +9,11 @@ namespace VroomStats.Services;
 public class DatabaseService : IDatabaseService
 {
     private readonly IMongoCollection<CarModel> _collection;
+    private readonly SemaphoreSlim _semaphore;
 
     public DatabaseService(IMongoDatabase database)
     {
+        _semaphore = new SemaphoreSlim(1);
         _collection = database.GetCollection<CarModel>("car-data");
     }
 
@@ -44,59 +46,86 @@ public class DatabaseService : IDatabaseService
     /// <inheritdoc cref="IDatabaseService.AppendDataAsync"/>
     public async Task AppendDataAsync(string carId, BasePayload payload)
     {
-        var carData = await _collection
-            .Find(x => x.Id == carId)
-            .FirstOrDefaultAsync();
+        await _semaphore.WaitAsync();
 
-        if (carData is null)
+        try
         {
-            await RegisterCarAsync(carId, new CarSettingsModel(new Dictionary<string, string>()));
-            carData = await _collection
+            var carData = await _collection
                 .Find(x => x.Id == carId)
-                .FirstAsync();
+                .FirstOrDefaultAsync();
 
             if (carData is null)
             {
-                throw new InvalidOperationException("Car not found.");
-            }
-        }
+                await RegisterCarAsync(carId, new CarSettingsModel(new Dictionary<string, string>()));
+                carData = await _collection
+                    .Find(x => x.Id == carId)
+                    .FirstAsync();
 
-        carData.Data.Add(new CarDataModel(DateTimeOffset.Now, payload.Data));
-        await _collection.ReplaceOneAsync(x => x.Id == carId, carData);
+                if (carData is null)
+                {
+                    throw new InvalidOperationException("Car not found.");
+                }
+            }
+
+            carData.Data.Add(new CarDataModel(DateTimeOffset.Now, payload.Data));
+            await _collection.ReplaceOneAsync(x => x.Id == carId, carData);
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
     }
 
     /// <inheritdoc cref="IDatabaseService.UpdateSettingsAsync"/>
     public async Task<CarOutModel?> UpdateSettingsAsync(string carId, CarSettingsModel settings)
     {
-        var carData = await _collection
-            .Find(x => x.Id == carId)
-            .FirstOrDefaultAsync();
-
-        if (carData is null)
-        {
-            return null;
-        }
-
-        carData.Settings.Clear();
-        foreach (var (key, value) in settings.Settings)
-        {
-            carData.Settings.Add(key, value);
-        }
+        await _semaphore.WaitAsync();
         
-        await _collection.ReplaceOneAsync(x => x.Id == carId, carData);
-        return new CarOutModel(carData.Id, carData.Settings);
+        try
+        {
+            var carData = await _collection
+                .Find(x => x.Id == carId)
+                .FirstOrDefaultAsync();
+
+            if (carData is null)
+            {
+                return null;
+            }
+
+            carData.Settings.Clear();
+            foreach (var (key, value) in settings.Settings)
+            {
+                carData.Settings.Add(key, value);
+            }
+        
+            await _collection.ReplaceOneAsync(x => x.Id == carId, carData);
+            return new CarOutModel(carData.Id, carData.Settings);
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
     }
     
     /// <inheritdoc cref="IDatabaseService.RegisterCarAsync"/>
     public async Task<bool> RegisterCarAsync(string carId, CarSettingsModel settings)
     {
-        if (await _collection.Find(x => x.Id == carId).AnyAsync())
+        await _semaphore.WaitAsync();
+        
+        try
         {
-            return false;
-        }
+            if (await _collection.Find(x => x.Id == carId).AnyAsync())
+            {
+                return false;
+            }
 
-        await _collection.InsertOneAsync(new CarModel(carId, settings.Settings, new List<CarDataModel>()));
-        return true;
+            await _collection.InsertOneAsync(new CarModel(carId, settings.Settings, new List<CarDataModel>()));
+            return true;
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
     }
     
     /// <inheritdoc cref="IDatabaseService.GetLatestDataAsync"/>
